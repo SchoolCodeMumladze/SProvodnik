fa6d6521a245a42e951cd27e4fd87b51cfd58491
Saveliy <saveliy.tgn@gmail.com>
2026-05-10 16:35:04 +0300

    Версия 0.2, вводится TKinter, ПРОГРАММА НЕРАБОЧАЯ

--- SProvodnik.py
+++ SProvodnik.py
@@ -5,84 +5,80 @@ import tkinter as tk
 def type_analyze(lis):
     for _ in lis:
         if os.path.isfile(_):
-            # name = tk.Label(window, text="Файл")
-            # name.pack()
-            print("Файл")
-            print(_)
-            # btn = tk.Button(window, f_analyze)
-            # btn.pack()
-            # f_analyze()
-            # break
+            btn = tk.Button(window, text=f"Файл {_}", command=f_analyze)
+            btn.pack()
         else:
-            # name = tk.Label(window, text="Папка")
-            # name.pack()
-            print("Папка")
-            print(_)
-            # btn = tk.Button(window, d_analyze)
-            # d_analyze()
+            btn = tk.Button(window, text=f"Папка {_}", command=d_analyze)
+            btn.pack()
 
 def f_analyze():
-    # new_window = tk.Toplevel()
-    # b_1 = tk.Button(new_window, text="Открыть файл", command=open_file)
-    # b_1.pack()
-    # b_2 = tk.Button(new_window, text="Удалить файл", command=remove_file)
-    # b_2.pack()
-    inp = input("Введите О, если хотите открыть файл, введите У, если хотите удалить файл или введите П, если хотите переименовать файл ")
-    if inp == "О":
-        open_file()
-    elif inp == "У":
-        remove_file()
-    elif inp == "С":
-        rename_file()
+    new_window = tk.Toplevel()
+    b_1 = tk.Button(new_window, text="Открыть файл", command=open_file)
+    b_1.pack()
+    b_2 = tk.Button(new_window, text="Удалить файл", command=remove_file)
+    b_2.pack()
+    b_3 = tk.Button(new_window, text="Переименовать файл", command=rename_file)
+    b_3.pack()
+    new_window.mainloop()
 
 def d_analyze():
-    # new_window = tk.Toplevel()
-    # b_1 = tk.Button(new_window, text="Открыть папку", command=open_dir)
-    # b_1.pack()
-    # b_2 = tk.Button(new_window, text="Удалить папку", command=remove_file)
-    # b_2.pack()
-    inp = input("Введите О, если хотите открыть папку, введите У, если хотите удалить папку, введите П, если хотите переименовать папку ")
-    if inp == "О":
-        open_dir()
-    elif inp == "У":
-        remove_file()
-    else:
-        rename_file()
+    new_window = tk.Toplevel()
+    b_1 = tk.Button(new_window, text="Открыть папку", command=open_dir)
+    b_1.pack()
+    b_2 = tk.Button(new_window, text="Удалить папку", command=remove_file)
+    b_2.pack()
+    b_3 = tk.Button(new_window, text="Переименовать папку", command=rename_file)
+    b_3.pack()
+    new_window.mainloop()
 
 def open_dir():
-    os.chdir(os.getcwd())
+    global name_file
+    os.chdir(name_file)
+    type_analyze(os.listdir())
 
 def open_file():
-    os.chdir(os.getcwd())
+    global name_file
+    os.chdir(name_file)
     os.startfile(os.getcwd())
 
 def rename_file():
     inp = os.getcwd().split("\\")[-1]
-    os.rename(inp, input("Введите новое имя файла/папки "))
+    new_name = tk.Entry(window)
+    new_name.pack()
+    os.rename(inp, new_name)
 
 def remove_file():
-    os.remove(os.getcwd())
-    print("Вы успешно удалили папку / файл")
+    inp = os.getcwd().split("\\")[-1]
+    os.remove(inp)
+    t = tk.Label(text="Вы успешно удалили файл/папку!")
+    t.pack()
 
-# window = tk.Tk()
-# window.geometry('500x2000')
-# t = tk.Label(window, text="Добро пожаловать в SПроводник",
-#                 font=100,
-#                 foreground="black",
-#                 background="white",
-#                 height=2)
-# t.pack()
-print("Добро пожаовать в SПроводник")
-disc = input("Введите букву диска ")
+def get_entry():
+    name_file.get()
+    window.config()
+
+window = tk.Tk()
+window.geometry('500x2000')
+t = tk.Label(window, text="Добро пожаловать в SПроводник",
+                font=100,
+                foreground="black",
+                background="white",
+                height=2)
+t.pack()
+# print("Добро пожаовать в SПроводник")
+# disc = input("Введите букву диска ")
 direction = os.getcwd()
 os.chdir(direction[2])
 direction = os.getcwd()
-# while True:
 lis = os.listdir()
 type_analyze(lis)
 
 while True:
-    name_file = input("Введите название файла/папки, с которым хотите работать ")
+    name_file = tk.Entry(window)
+    name_file.pack()
+    bttn = tk.Button(window, text="Подтвердить текст", command=get_entry)
+    bttn.pack()
+    window.mainloop()
     if name_file not in os.listdir():
         open(name_file, "w")
     if os.path.isfile(name_file):
@@ -92,5 +88,3 @@ while True:
         os.chdir(name_file)
         d_analyze()
         type_analyze(os.listdir())
-
-# window.mainloop()
